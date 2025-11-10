4820b85450facc28577af28fa6afbb491d2f8a27
woojo230 <woojunghyck@naver.com>
2025-11-10 20:48:01 +0900

    docs: Update copy for About page

--- src/pages/about/components/Left.jsx
+++ src/pages/about/components/Left.jsx
@@ -20,11 +20,23 @@ function Left() {
       <div className={styles.aboutPageDiv}>
         <p className={styles.sectionLabel}>Our Mission</p>
         <h1>
-          As designers, we've learned that creating beautiful work is essential,
-          but maintaining consistency within established design systems can be
-          challenging and labor intensive. That's why we built Tooltool: to make
-          it simple and efficent for anyone who needs a design assets for their
-          brands.
+          A new design era has arrived! Graphics are now easily created by code,
+          with designers building tools with the help of LLMs. While generating
+          visuals has never been easier, it takes another step of curation,
+          design thinking, and craft to turn these tools into branding and
+          real-world projects. At Tooltool, our mission is to showcase creative
+          code-based tools in action, inspiring designers as they implement them
+          for their future projects.
+        </h1>
+        <h1>
+          Tooltool is a showcase platform where tools and designers come
+          together. That is why our name is not a singular “tool” but
+          “tooltool,” a plural form that reflects community. The tools we share
+          include a series of graphics, typography, patterns, effects, and more.
+          Whether you’re here to play or to spark ideas for your next project,
+          you’re always welcome! And if you’re a designer building your own
+          tools, we’d love to showcase your work. Join our showcase and
+          community of designers shaping the next design era!
         </h1>
       </div>
     </div>
